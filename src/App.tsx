83caa275230fd96dde3831617a498800ba347fb2
Kayke Barbosa <kaykeloiola@gmail.com>
2022-12-11 11:10:12 -0300

    4 commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Private } from './pages/Private';
+import { RequireAuth } from './contexts/Auth/RequireAuth';
+import { AuthContext } from './contexts/Auth/AuthContext';
+
+function App() {
+  const auth = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    await auth.signout();
+    window.location.href = window.location.href;
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Header do site</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Página Privada</Link>
+          {auth.user && <button onClick={handleLogout}>Sair</button>}
+        </nav>
+      </header>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<RequireAuth><Private /></RequireAuth>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
